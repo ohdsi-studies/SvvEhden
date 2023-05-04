0e3d231da3569ae968ddb611be21b28288312ca9
Sara Vidlin <sara.vidlin@who-umc.org>
2023-05-04 09:53:04 +0200

    cleanup

--- inst/sql/sql_server/ExtractChronographTable.sql
+++
@@ -1,7 +0,0 @@
-{DEFAULT @cohort_database_schema = ''}
-
-SELECT exposed.period_id, exposed_count, comparator_count, outcome_count, comparator_outcome_count FROM @cohort_database_schema.exp_ICTPD exposed 
-LEFT JOIN @cohort_database_schema.comp_ICTPD comparator ON exposed.period_id = comparator.period_id
-LEFT JOIN @cohort_database_schema.exp_out_ICTPD exposed_o ON exposed.period_id = exposed_o.period_id
-LEFT JOIN @cohort_database_schema.comp_out_ICTPD comparator_o ON exposed.period_id = comparator_o.period_id
-ORDER BY exposed.PERIOD_ID DESC
--- man-roxygen/VocabularyDatabaseSchema.R
+++
@@ -1,4 +0,0 @@
-#' @param vocabularyDatabaseSchema   Schema name where your OMOP vocabulary data resides. This is 
-#'                                   commonly the same as cdmDatabaseSchema. Note that for 
-#'                                   SQL Server, this should include both the database and
-#'                                   schema name, for example 'vocabulary.dbo'.
